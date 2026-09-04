388a3aad7cc45a7385101cf3094845733527e436
uhuuhuuhuhu <since2021sm@gmail.com>
2026-09-05 01:51:45 +0900

    Create sitemap.js

---
+++ sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://compound-calculator-khaki.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1.0,
+    },
+  ];
+}
